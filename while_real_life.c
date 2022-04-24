c3bfeda801bd4699103b61307a20e31481f2133b
Md Mahmudul Hasan <51690967+hasanmdmahmudul@users.noreply.github.com>
2022-04-24 11:07:36 +1000

    Add files via upload

---
+++ while_real_life.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int employee_code=1;
+    while(employee_code<=500)
+    {
+        printf("Email to employee: %d\n",employee_code);
+        employee_code++;
+    }
+
+    return 0;
+}
+
+
